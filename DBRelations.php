d7f15ecaaeeed1baaef27d326d33da8f58c94b9b
Van Nguyen <soutennizavan@gmail.com>
2015-04-15 16:39:17 -0400

    more stuff

--- DBRelations.php
+++ DBRelations.php
@@ -22,11 +22,20 @@
     1=>'tinyint',
     2=>'smallint',
     3=>'int',
+    4=>'float',
     5=>'double',
+    7=>'timestamp',
+    8=>'bigint',
     9=>'mediumint',
     10=>'date',
+    11=>'time',
+    12=>'datetime',
+    13=>'year',
+    16=>'bit',
+    252=>'text',
     253=>'varchar',
-    254=>'char');
+    254=>'char',
+    246=>'decimal');
 
 
     $fieldinfo = mysqli_fetch_fields($result);
@@ -116,8 +125,109 @@
               if(isset($_GET['student'])){
                 relations('Student');
               }
+
               if(isset($_GET['person'])){
                 relations('Person');
+              }
+
+              if(isset($_GET['faculty'])){
+                relations('Faculty');
+              }
+
+              if(isset($_GET['academic'])){
+                relations('Academic');
+              }
+
+              if(isset($_GET['administrator'])){
+                relations('Administrator');
+              }
+
+              if(isset($_GET['cd'])){
+                relations('CD');
+              }
+
+              if(isset($_GET['coach'])){
+                relations('Coach');
+              }
+
+              if(isset($_GET['course'])){
+                relations('Course');
+              }
+
+              if(isset($_GET['college'])){
+                relations('College');
+              }
+
+              if(isset($_GET['currentSection'])){
+                relations('Current_Section');
+              }
+
+              if(isset($_GET['department'])){
+                relations('Department');
+              }
+
+              if(isset($_GET['grant'])){
+                relations('Grantt');
+              }
+
+              if(isset($_GET['instructorResearcher'])){
+                relations('Instructor_Researcher');
+              }
+
+              if(isset($_GET['major'])){
+                relations('Major');
+              }
+
+              if(isset($_GET['minor'])){
+                relations('Minor');
+              }
+
+              if(isset($_GET['nonacademic'])){
+                relations('Non_Academic');
+              }
+
+              if(isset($_GET['offered'])){
+                relations('Offered');
+              }
+
+              if(isset($_GET['primary'])){
+                relations('Primaryy');
+              }
+
+              if(isset($_GET['grade'])){
+                relations('Grade');
+              }
+
+              if(isset($_GET['primary'])){
+                relations('Primaryy');
+              }
+
+              if(isset($_GET['registered'])){
+                relations('Registered');
+              }
+
+              if(isset($_GET['secondary'])){
+                relations('Secondary');
+              }
+
+              if(isset($_GET['section'])){
+                relations('Section');
+              }
+
+              if(isset($_GET['sport'])){
+                relations('Sport');
+              }
+
+              if(isset($_GET['staff'])){
+                relations('Staff');
+              }
+
+              if(isset($_GET['teaches'])){
+                relations('Teaches');
+              }
+
+              if(isset($_GET['transcript'])){
+                relations('Transcript');
               }
             ?>
           </div>
@@ -133,67 +243,100 @@
           <a href='DBRelations.php?person=true'class="btn btn-primary">Person</a>
         </div>
         <div class="col-md-4">
-          <a class="btn btn-primary">Inset Relation Here</a>
+          <a href='DBRelations.php?faculty=true' class="btn btn-primary">Faculty</a>
         </div>
       </div>
       <hr>
       <div class="row">
         <div class="col-md-4">
-          <a class="btn btn-primary">Insert Relation Here</a>
+          <a href='DBRelations.php?academic=true'class="btn btn-primary">Academic</a>
         </div>
         <div class="col-md-4">
-          <a class="btn btn-primary">Insert Relation Here</a>
+          <a href='DBRelations.php?administrator=true'class="btn btn-primary">Administrator</a>
         </div>
         <div class="col-md-4">
-          <a class="btn btn-primary">Insert Relation Here</a>
-        </div>
-      </div>
-      <hr>
-      <div class="row">
-        <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
-        </div>
-        <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
-        </div>
-        <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
+          <a href='DBRelations.php?cd=true'class="btn btn-primary">CD</a>
         </div>
       </div>
       <hr>
       <div class="row">
         <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
+          <a href='DBRelations.php?coach=true' class="btn btn-primary">Coach</a>
         </div>
         <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
+          <a href='DBRelations.php?college=true' class="btn btn-primary">College</a>
         </div>
         <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
-        </div>
-      </div>
-      <hr>
-      <div class="row">
-        <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
-        </div>
-        <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
-        </div>
-        <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
+          <a href='DBRelations.php?course=true' class="btn btn-primary">Course</a>
         </div>
       </div>
       <hr>
       <div class="row">
         <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
+          <a href='DBRelations.php?currentSection=true' class="btn btn-primary">Current_Section</a>
         </div>
         <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
+          <a href='DBRelations.php?department=true' class="btn btn-primary">Department</a>
         </div>
         <div class="col-md-4">
-          <a class="btn btn-primary">Inser Relatin Here</a>
+          <a href='DBRelations.php?grade=true' class="btn btn-primary">Grade</a>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-4">
+          <a href='DBRelations.php?grant=true' class="btn btn-primary">Grant</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?instructorResearcher=true' class="btn btn-primary">Instructor_Researcher</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?major=true' class="btn btn-primary">Major</a>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-4">
+          <a href='DBRelations.php?minor=true' class="btn btn-primary">Minor</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?nonacademic=true' class="btn btn-primary">Non_Academic</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?offered=true' class="btn btn-primary">Offered</a>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-4">
+          <a href='DBRelations.php?primary=true' class="btn btn-primary">Primary</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?registered=true' class="btn btn-primary">Registered</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?secondary=true' class="btn btn-primary">Secondary</a>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-4">
+          <a href='DBRelations.php?section=true' class="btn btn-primary">Section</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?sport=true' class="btn btn-primary">Sport</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?staff=true' class="btn btn-primary">Staff</a>
+        </div>
+      </div>
+      <hr>
+      <div class="row">
+        <div class="col-md-4">
+          <a href='DBRelations.php?teaches=true' class="btn btn-primary">Teaches</a>
+        </div>
+        <div class="col-md-4">
+          <a href='DBRelations.php?transcript=true' class="btn btn-primary">Transcript</a>
         </div>
       </div>
     </div>
