01da2c1cc65c7ebc9b7388e1eae9e6c912ad1ae9
Van Nguyen <soutennizavan@gmail.com>
2015-04-14 23:03:58 -0400

    Database populated

--- DBDatabase.php
+++ DBDatabase.php
@@ -11,61 +11,176 @@
     }
 
     $sql = "INSERT INTO Student (SNum, SSN, Class)
-    VALUES (100000 , 123322123, '3')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Student (SNum, SSN, Class)
-    VALUES (100001 , 123123123, '3')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Student (SNum, SSN, Class)
-    VALUES (100002 , 123456789, '4')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Student (SNum, SSN, Class)
-    VALUES (100003 , 981981981, '1')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Student (SNum, SSN, Class)
-    VALUES (100004 , 823123321, '4')";
-    $conn->query($sql);
+    VALUES (100000 , 200200200, '3');";
+    $sql .= "INSERT INTO Student (SNum, SSN, Class)
+    VALUES (100001 , 200200201, '3');";
+    $sql .= "INSERT INTO Student (SNum, SSN, Class)
+    VALUES (100002 , 200200202, '4');";
+    $sql .= "INSERT INTO Student (SNum, SSN, Class)
+    VALUES (100003 , 200200203, '1');";
+    $sql .= "INSERT INTO Student (SNum, SSN, Class)
+    VALUES (100004 , 200200204, '4');";
 
-    $sql = "INSERT INTO Major (SSN, DName, DCode)
-    VALUES (823123321 , 'Computer Science', '1')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Major (SSN, DName, DCode)
-    VALUES (981981981 , 'English', '2')";
-    $conn->query($sql);
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (200200200, 'Liam Neesons', '1001-12-25', '123 Liam Way', 0);";
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (200200201, 'Thorin Oakenshield', '1321-11-25', '124 Liam Way', 0);";
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (200200202, 'Tony Stark', '1433-01-5', '125 Greatwood Way', 0);";
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (200200203, 'Margaery Tyrell', '1643-03-28', '124 Greatwood Way', 1);";
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (200200204, 'Daenerys Targaryen', '1337-12-23', '124 Drogo Lane', 1);";
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (100100100, 'Jon Snow', '1437-02-23', '14 Castle Black Street', 0);";
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (100100123, 'Arya Stark', '1637-12-01', '4 Pentos Mile', 1);";
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (100123100, 'Oliver Queen', '1927-06-23', '124 Verdant Parkway', 0);";
+    $sql .= "INSERT INTO Person (SSN, Name, BDate, Address, Sex)
+    VALUES (123100100, 'Bruce Wayne', '1999-08-11', '124 Wayne Estate', 0);";
 
-    $sql = "INSERT INTO Faculty (SSN, Rank, FOffice, FPhone, Salary)
-    VALUES (100100100 , '1', '123 Van Lane', '3135867855', '100000')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Faculty (SSN, Rank, FOffice, FPhone, Salary)
-    VALUES (100100123 , '2', '123 Kelly Lane', '3135867866', '200000')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Faculty (SSN, Rank, FOffice, FPhone, Salary)
-    VALUES (100123100 , '1', '124 Van Lane', '3135867877', '150000')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Faculty (SSN, Rank, FOffice, FPhone, Salary)
-    VALUES (100100123 , '3', '124 Kelly Lane', '3135867888', '690000')";
-    $conn->query($sql);
+    $sql .= "INSERT INTO Faculty (SSN, Rank, FOffice, FPhone, Salary)
+    VALUES (100100100 , '1', '123 Van Lane', '3135867855', '100000');";
+    $sql .= "INSERT INTO Faculty (SSN, Rank, FOffice, FPhone, Salary)
+    VALUES (100100123 , '2', '123 Kelly Lane', '3135867866', '200000');";
+    $sql .= "INSERT INTO Faculty (SSN, Rank, FOffice, FPhone, Salary)
+    VALUES (100123100 , '1', '124 Van Lane', '3135867877', '13370000');";
+    $sql .= "INSERT INTO Faculty (SSN, Rank, FOffice, FPhone, Salary)
+    VALUES (123100100 , '3', '124 Kelly Lane', '3135867888', '690000');";
 
-    $sql = "INSERT INTO Minor (SSN, DName, DCode)
-    VALUES (823123321 , 'English', '2')";
-    $conn->query($sql);
-    $sql = "INSERT INTO Minor (SSN, DName, DCode)
-    VALUES (981981981 , 'Computer Science', '1')";
-    $conn->query($sql);
+    $sql .= "INSERT INTO Major (SSN, DName, DCode)
+    VALUES (200200204 , 'Computer Science', '1');";
+    $sql .= "INSERT INTO Major (SSN, DName, DCode)
+    VALUES (200200203 , 'English', '2');";
 
-    $sql = "INSERT INTO Department (DName, DCode, DPhone, DNumber)
-    VALUES ('English', '2', 3125648293, 222)";
-    $conn->query($sql);
-    $sql = "INSERT INTO Department (DName, DCode, DPhone, DNumber)
-    VALUES ('Computer Science', '1', 3125648294, 111)";
+    $sql .= "INSERT INTO Minor (SSN, DName, DCode)
+    VALUES (200200204 , 'English', '2');";
+    $sql .= "INSERT INTO Minor (SSN, DName, DCode)
+    VALUES (200200203 , 'Computer Science', '1');";
 
-    $sql = "INSERT INTO College (CName, COffice, CDean)
-    VALUES ('Engineering', '6900 Kelly Road', 'Kelly Markait')";
-    $sql = "INSERT INTO College (CName, COffice, CDean)
-    VALUES ('Arts', '6900 Van Road', 'Van Nguyen')";
-    $conn->query($sql);
+    $sql .= "INSERT INTO Department (DName, DCode, DPhone, DNumber)
+    VALUES ('English', '2', 3125648293, 222);";
+    $sql .= "INSERT INTO Department (DName, DCode, DPhone, DNumber)
+    VALUES ('Computer Science', '1', 3125648294, 111);";
 
-    if($conn->query($sql) === TRUE){
+    $sql .= "INSERT INTO College (CName, COffice, CDean)
+    VALUES ('Engineering', '6900 Kelly Road', 'Kelly Markaity');";
+    $sql .= "INSERT INTO College (CName, COffice, CDean)
+    VALUES ('Arts', '6900 Van Road', 'Van Nguyen');";
+
+    $sql .= "INSERT INTO Section (SecNum, Semster, InstructorName, Year, SecCourse)
+    VALUES ('100', 'FALL', 'Jon Snow', 2102, 123);";
+    $sql .= "INSERT INTO Section (SecNum, Semster, InstructorName, Year, SecCourse)
+    VALUES ('101', 'WINTER', 'Arya Stark', 2103, 124);";
+
+    $sql .= "INSERT INTO Course (CNum, CName, CDesc, Credit, Level, CDept)
+    VALUES ('100', 'Intro to Databases', 'Learn about bases of data!', 3, 4, 'Computer Science');";
+    $sql .= "INSERT INTO Course (CNum, CName, CDesc, Credit, Level, CDept)
+    VALUES ('101', 'Intro to Beer Making', 'Beers', 2, 1, 'English');";
+
+    $sql .= "INSERT INTO Current_Section (SecNum, CNum, Semester, Year)
+    VALUES ('100', '100', 'FALL', 2102);";
+    $sql .= "INSERT INTO Current_Section (SecNum, CNum, Semester, Year)
+    VALUES ('101', '101', 'WINTER', 2103);";
+
+    $sql .= "INSERT INTO Grade (SSN, Grade, SecNum)
+    VALUES (200200200, 'A', 100);";
+    $sql .= "INSERT INTO Grade (SSN, Grade, SecNum)
+    VALUES (200200201, 'B', 100);";
+    $sql .= "INSERT INTO Grade (SSN, Grade, SecNum)
+    VALUES (200200202, 'C', 100);";
+    $sql .= "INSERT INTO Grade (SSN, Grade, SecNum)
+    VALUES (200200203, 'A', 100);";
+    $sql .= "INSERT INTO Grade (SSN, Grade, SecNum)
+    VALUES (200200202, 'A', 101);";
+    $sql .= "INSERT INTO Grade (SSN, Grade, SecNum)
+    VALUES (200200203, 'A', 101);";
+
+    $sql .= "INSERT INTO Grantt (No, Title, Agency, St_Date)
+    VALUES (100, 'Beer Research', 'Vaniel Charities', '1999-03-21');";
+    $sql .= "INSERT INTO Grantt (No, Title, Agency, St_Date)
+    VALUES (101, 'Arrow Research', 'Kelly Charities', '1999-03-21');";
+
+    $sql .= "INSERT INTO Instructor_Researcher (SSN)
+    VALUES (100100100);";
+    $sql .= "INSERT INTO Instructor_Researcher (SSN)
+    VALUES (100100123);";
+    $sql .= "INSERT INTO Instructor_Researcher (SSN)
+    VALUES (100123100);";
+    $sql .= "INSERT INTO Instructor_Researcher (SSN)
+    VALUES (123100100);";
+
+    $sql .= "INSERT INTO Non_Academic (SSN)
+    VALUES (100100100);";
+    $sql .= "INSERT INTO Non_Academic (SSN)
+    VALUES (100123100);";
+    $sql .= "INSERT INTO Non_Academic (SSN)
+    VALUES (123100100);";
+
+    $sql .= "INSERT INTO Offered (SecNum, CNum)
+    VALUES (100, 100);";
+    $sql .= "INSERT INTO Offered (SecNum, CNum)
+    VALUES (101, 101);";
+
+    $sql .= "INSERT INTO Primaryy (Nos, SNum, SSN)
+    VALUES ('Rock Climbing', 100000, 200200200);";
+    $sql .= "INSERT INTO Primaryy (Nos, SNum, SSN)
+    VALUES ('Hockey', 100001, 200200201);";
+
+    $sql .= "INSERT INTO Secondary (Nos, SNum, SSN)
+    VALUES ('Rock Climbing', 100001, 200200201);";
+    $sql .= "INSERT INTO Secondary (Nos, SNum, SSN)
+    VALUES ('Hockey', 100000, 200200200);";
+
+    $sql .= "INSERT INTO Registered (SSN, SecNum)
+    VALUES (200200200, 100);";
+    $sql .= "INSERT INTO Registered (SSN, SecNum)
+    VALUES (200200200, 101);";
+
+    $sql .= "INSERT INTO Sport (PIC, OffLoc, NoS)
+    VALUES ('Van Nguyen', '123 Van Way', 'Rock Climbing');";
+    $sql .= "INSERT INTO Sport (PIC, OffLoc, NoS)
+    VALUES ('Kelly Markaity', '123 Kelly Way', 'Hockey');";
+
+    $sql .= "INSERT INTO Staff (SSN)
+    VALUES (100100123);";
+
+    $sql .= "INSERT INTO Teaches (SSN, SecNum)
+    VALUES (100100100, 100);";
+    $sql .= "INSERT INTO Teaches (SSN, SecNum)
+    VALUES (100100123, 101);";
+
+    $sql .= "INSERT INTO Transcript (SSN, SecNum, SNum_Grade)
+    VALUES (200200200, 100, 'A');";
+    $sql .= "INSERT INTO Transcript (SSN, SecNum, SNum_Grade)
+    VALUES (200200201, 100, 'B');";
+    $sql .= "INSERT INTO Transcript (SSN, SecNum, SNum_Grade)
+    VALUES (200200202, 100, 'C');";
+    $sql .= "INSERT INTO Transcript (SSN, SecNum, SNum_Grade)
+    VALUES (200200203, 100, 'A');";
+    $sql .= "INSERT INTO Transcript (SSN, SecNum, SNum_Grade)
+    VALUES (200200202, 101, 'A');";
+    $sql .= "INSERT INTO Transcript (SSN, SecNum, SNum_Grade)
+    VALUES (200200203, 101, 'A');";
+
+    $sql .= "INSERT INTO Coach (SSN, NoS)
+    VALUES (100100123, 'Rock Climbing');";
+    $sql .= "INSERT INTO Coach (SSN, NoS)
+    VALUES (123100100, 'Hockey');";
+
+    $sql .= "INSERT INTO CD (CName, DName)
+    VALUES ('Engineering', 'Computer Science');";
+    $sql .= "INSERT INTO CD (CName, DName)
+    VALUES ('Arts', 'English');";
+
+    $sql .= "INSERT INTO Administrator (SSN, ACode, AName, APhone, AOffice)
+    VALUES (100123100, 'ENG' , 'AENG' , 3153151212, '12 Your Street');";
+
+    $sql .= "INSERT INTO Academic (SSN)
+    VALUES (123100100);";
+
+    if($conn->multi_query($sql) === TRUE){
       echo "<div class='alert alert-success'>Database Initialized</div>";
     }else {
       echo "<div class='alert alert-danger'>Database Initialization Error</div>";
@@ -83,22 +198,35 @@
       die("Connection failed: " . $conn.connect_error);
     }
 
-    $sql = "TRUNCATE TABLE Student";
-    $conn->query($sql);
+    $sql = "TRUNCATE TABLE Student;";
+    $sql .= "TRUNCATE TABLE Major;";
+    $sql .= "TRUNCATE TABLE Faculty;";
+    $sql .= "TRUNCATE TABLE Minor;";
+    $sql .= "TRUNCATE TABLE Department;";
+    $sql .= "TRUNCATE TABLE College;";
+    $sql .= "TRUNCATE TABLE Person;";
+    $sql .= "TRUNCATE TABLE Section;";
+    $sql .= "TRUNCATE TABLE Course;";
+    $sql .= "TRUNCATE TABLE Current_Section;";
+    $sql .= "TRUNCATE TABLE Grade;";
+    $sql .= "TRUNCATE TABLE Grantt;";
+    $sql .= "TRUNCATE TABLE Instructor_Researcher;";
+    $sql .= "TRUNCATE TABLE Non_Academic;";
+    $sql .= "TRUNCATE TABLE Offered;";
+    $sql .= "TRUNCATE TABLE Primaryy;";
+    $sql .= "TRUNCATE TABLE Secondary;";
+    $sql .= "TRUNCATE TABLE Registered;";
+    $sql .= "TRUNCATE TABLE Sport;";
+    $sql .= "TRUNCATE TABLE Staff;";
+    $sql .= "TRUNCATE TABLE Teaches;";
+    $sql .= "TRUNCATE TABLE Transcript;";
+    $sql .= "TRUNCATE TABLE Coach;";
+    $sql .= "TRUNCATE TABLE CD;";
+    $sql .= "TRUNCATE TABLE Administrator;";
+    $sql .= "TRUNCATE TABLE Academic;";
 
-    $sql = "TRUNCATE TABLE Major";
-    $conn->query($sql);
 
-    $sql = "TRUNCATE TABLE Faculty";
-    $conn->query($sql);
-
-    $sql = "TRUNCATE TABLE Minor";
-    $conn->query($sql);
-
-    $sql = "TRUNCATE TABLE Department";
-    $conn->query($sql);
-
-    if($conn->query($sql) === TRUE){
+    if($conn->multi_query($sql) === TRUE){
       echo "<div class='alert alert-success'>Database Cleared</div>";
     }else {
       echo "<div class='alert alert-danger'>Database Cleared Error</div>";
